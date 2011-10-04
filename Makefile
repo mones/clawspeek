59f84b854c342634e3ccb3c0bd55ff7b8d09b5a9
Ricardo Mones <ricardo@mones.org>
2011-10-04 23:02:28 +0200

    Public release

---
+++ Makefile
@@ -0,0 +1,43 @@
+#
+# Clawspeek makefile
+#
+NAME = clawspeek
+VERSION = 1.0
+
+PREFIX ?= /usr/local
+BINDIR = ${PREFIX}/bin
+DATADIR = ${PREFIX}/share
+MANDIR = ${DATADIR}/man
+MAN1DIR = ${MANDIR}/man1
+
+CC = gcc
+CFLAGS = $(shell pkg-config --cflags glib-2.0) 
+LIBS = $(shell pkg-config --libs glib-2.0)
+PCKF = passcrypt
+PCK = $(shell echo 'A2DD<6J_' | tr '\!-~' 'P-~\!-O')
+
+all: build
+
+build: $(NAME).c $(PCKF).h
+	$(CC) $(CFLAGS) -o $(NAME) base64.c $(PCKF).c $(NAME).c $(LIBS) -lcrypt
+
+$(PCKF).h: $(PCKF).h.in
+	@sed "s,@PCK@,$(PCK)," < $< > $@	
+
+clean:
+	rm -f $(NAME) $(PCKF).h
+
+install: all install-dirs
+	install -m 0755 ${NAME} ${DESTDIR}${BINDIR}
+	install -m 0644 ${NAME}.1 ${DESTDIR}${MAN1DIR}
+	
+install-dirs:
+	install -d ${DESTDIR}${BINDIR}
+	install -d ${DESTDIR}${MAN1DIR}
+
+uninstall:
+	rm -f ${DESTDIR}${BINDIR}/${NAME}
+	rm -f ${DESTDIR}${MAN1DIR}/${NAME}.1
+
+.PHONY: all build install install-dirs uninstall
+
---
+++ clawspeek.c
@@ -0,0 +1,38 @@
+/*
+ * Clawspeek -- minimalistic Claws Mail password recovery utility
+ * Copyright (C) 2011 Ricardo Mones and the Claws Mail team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+#include <glib.h>
+#include "passcrypt.h"
+
+int main(int argc, char*argv[])
+{
+	gchar *buf;
+	gint  len;
+
+	if (argc < 2) {
+		return -1;
+	}
+	buf = g_strnfill(1024, '\0');
+	len = base64_decode(buf, &argv[1][1], strlen(argv[1]) - 1);
+	passcrypt_decrypt(buf, len);
+	g_print("%s -> %s\n", argv[1], buf);
+	g_free(buf);
+	return 0;
+}
+
